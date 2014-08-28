252e2cbcb832a64c5082e19dd2a57b8c7e5c2d1d
Jesse McNelis <jesse.mcnelis@bugherd.com>
2014-08-28 16:46:07 +1000

    add stack strategy

---
+++ server/services/stack/index.js
@@ -0,0 +1,29 @@
+exports.setup = function (passport, config) {
+    var StackStrategy = require('passport-stack').Strategy;
+
+    passport.use(new StackStrategy({
+        clientID: config.STACK_CLIENT_ID,
+        clientSecret: config.STACK_CLIENT_SECRET,
+        callbackURL: config.STACK_CALLBACK_URL,
+        scope: config.STACK_SCOPE 
+      },
+      function(accessToken, refreshToken, profile, done) {
+            var user = {
+                refreshToken: refreshToken || "",
+                accessToken: accessToken,
+                provider: profile.provider,
+                id: profile.id,
+                uid: profile.provider + ':' + profile.id,
+                displayName: profile.name,
+                thirdPartyUserData: profile._json
+            };
+            return done(0, user);
+      }
+    ));
+
+    return {
+        options: {
+            "state": "_____"
+        }
+    };
+};
