ba6aaf31baf462ce513935ed12486fa23acba2bb
Nathan Hadfield <hadfieldn@gmail.com>
2016-04-14 07:41:18 -0600

    when login attempt is made without an anonymous FB connection, try to establish it for next login attempt

--- client/firebase-passport-login.js
+++ client/firebase-passport-login.js
@@ -1,5 +1,7 @@
 // Copyright (c) 2013 Abraham Haskins, https://github.com/abeisgreat https://github.com/rigidflame/firebase-passport-login
 var FirebasePassportLogin = (function (firebaseURL, oAuthServerURL, callback) {
+    'use strict';
+
     /* globals Firebase */
     var self = this;
     self._ref = new Firebase(firebaseURL);
@@ -13,14 +15,14 @@ var FirebasePassportLogin = (function (firebaseURL, oAuthServerURL, callback) {
     self._removeTokensImmediately = true;       // Change to false to persist user data at /oAuth/users/$token
 
 
-    self._popupCenter = function (url, title, w, h) {
+    function _popupCenter (url, title, w, h) {
         // from http://stackoverflow.com/questions/4068373/center-a-popup-window-on-screen
         // Fixes dual-screen position                         Most browsers      Firefox
         var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
         var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
 
-        width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-        height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+        var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+        var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
 
         var left = ((width / 2) - (w / 2)) + dualScreenLeft;
         var top = ((height / 2) - (h / 2)) + dualScreenTop;
@@ -32,7 +34,7 @@ var FirebasePassportLogin = (function (firebaseURL, oAuthServerURL, callback) {
         }
 
         return newWindow;
-    };
+    }
 
     /**
      * Set up Firebase listener on the anonymous user. When the anonymous user has been
@@ -40,11 +42,11 @@ var FirebasePassportLogin = (function (firebaseURL, oAuthServerURL, callback) {
      * obtained through Passport.
      * @private
      */
-    self._initializePassportLogin = function (oAuthTokenPath) {
+    function _initializeFirebaseOAuthUserListener (oAuthTokenPath) {
         var oAuthTokenRef = self._ref.child(oAuthTokenPath);
 
-        cookie.set('passportAnonymous', self._anonymousUid, {secure: document.location.href.indexOf("https") === 0});
-        oAuthTokenRef.on("value", function (snapshot) {
+        cookie.set('passportAnonymous', self._anonymousUid, {secure: document.location.href.indexOf('https') === 0});
+        oAuthTokenRef.on('value', function (snapshot) {
             var token = snapshot.val();
             //('value changed for auth token: ' + token);
             if (token) {
@@ -59,12 +61,12 @@ var FirebasePassportLogin = (function (firebaseURL, oAuthServerURL, callback) {
                 }
                 self._ref.unauth();
 
-                self._setToken(token);
+                _getUserForTokenAndSaveSession(token);
             }
         });
 
 
-    };
+    }
 
     /**
      * Authenticate to Firebase with the custom token obtained through OAuth and store it as a
@@ -73,7 +75,7 @@ var FirebasePassportLogin = (function (firebaseURL, oAuthServerURL, callback) {
      * @param token - Passport session token
      * @private
      */
-    self._setToken = function (token) {
+    function _getUserForTokenAndSaveSession (token) {
         if (!token) return;
         self._ref.authWithCustomToken(token, function (error, data) {
             if (error) {
@@ -100,89 +102,45 @@ var FirebasePassportLogin = (function (firebaseURL, oAuthServerURL, callback) {
                 });
             }
         });
-    };
+    }
 
-    self._log = function (message) {
+    function _log (message) {
         if (console.log) {
             console.log("FirebasePassportLogin: " + message);
         }
-    };
-
-    self._oAuthTokenPath = function() {
-        return [
-            self._tokenPath,
-            self._anonymousUid
-        ].join('/');
-    };
+    }
 
     /**
-     * Log in to Firebase through the indicated authentication provider.
+     * Firebase path to which the authenticated user data should be written. It includes the uid of the
+     * anonymous user we use to listen for the authenticated user object.
      *
-     * First creates an anonymous authentication with Firebase, passing the anonymous auth
-     * token on to the server along with a Firebase key to set with authenticated user data.
-     * We then listen for the authenticated user data to be set, calling the user-provided
-     * `callback` function when it is.
+     * @param {String} anonymousUid - UID of anonymous Firebase session used to listen for the authenticated user
+     * object obtained through Passport by the backend auth service
+     * @returns {string} - A Firebase path (not a ref) that can be passed to the backend auth service to let it know
+     * where to store the user data obtained through Passport.
      *
-     * The authenticated user token is stored in a cookie (`passportCookie`) to enable
-     * automatic authentication later.
-     *
-     * @param {String} provider - Provider to authenticate through ('facebook', 'reddit', etc.)
+     * @private
      */
-    self.login = function (provider) {
+    function _firebaseOAuthUserPath (anonymousUid) {
+      return self._tokenPath + '/' + anonymousUid;
+    }
 
-        // Have to open the authentication window immediately in order to avoid popup blockers
-
-        self._provider = provider;
-        if (! self._anonymousUid) {
-            // _getAnonymousUid() should have been called automatically when the page loaded.
-            self._log("Anonymous login failed. Make sure Anonymous login is enabled in your Firebase");
-        }
-        else {
-            var oAuthTokenPath = self._oAuthTokenPath();
-            var oAuthWindowURL = self._oAuthServerURL + self._provider
-              + "?oAuthTokenPath=" + oAuthTokenPath
-              + "&redirect=" + encodeURIComponent(self._redirectURL);
-            if (self._firebaseURL) {
-                oAuthWindowURL += "&firebaseURL=" + encodeURIComponent(self._firebaseURL);
-            }
-            self._oAuthWindow = self._popupCenter(oAuthWindowURL, "_blank",
-              self._oAuthServerWindow.width,
-              self._oAuthServerWindow.height);
-
-            self._initializePassportLogin(oAuthTokenPath);
-        }
-    };
-
-    self._getAnonymousUid = function () {
+    function _getAnonymousUid () {
         if (self._ref.getAuth()) {
             self._ref.unauth();
         }
-        self._ref.authAnonymously(function (err, authData) {
-            if (err) {
-                self._log("Anonymous login failed. Make sure Anonymous login is enabled in your Firebase");
-            } else {
-                var user = authData.auth;
-                if (user) {
-                    self._anonymousUid = user.uid;
-                }
+        return self._ref.authAnonymously({remember: 'default'})
+          .then(function (authData) {
+            var user = authData.auth;
+            if (user) {
+              return user.uid;
             }
+          })
+          .catch(function (err) {
+            throw new Error("Anonymous login failed. Make sure Anonymous login is enabled in your Firebase");
+          });
 
-        }, {remember: 'default'});   // remember: 'none', 'sessionOnly', 'default'
-    };
-
-    /**
-     * Disconnect from Firebase and clear the passportSession cookie.
-     */
-    self.logout = function () {
-        var token = cookie.get("passportSession");
-        if (token) {
-            self._ref.child('oAuthUsers').child(token.replace(/\./g, '')).remove();
-        }
-        self._ref.unauth();
-        cookie.set("passportSession", "");
-        self._callback(null, null);
-    };
-
+    }
 
     /**
      * To re-establish an anonymous connection to the database prior to calling login,
@@ -193,29 +151,46 @@ var FirebasePassportLogin = (function (firebaseURL, oAuthServerURL, callback) {
      * where 'login-iframe' is the id of the iframe containing the login popup window.
      *
      */
-    self._messageHandler = function (event) {
+    function _messageHandler (event) {
+      var action;
+
       try {
-        var action = JSON.parse(event.data);
-        self._getAnonymousUid();
-        if (action.redirect) {
-          self._redirectURL = action.redirect;
-        }
+        action = JSON.parse(event.data);
       } catch (err) {
         // couldn't parse the event data -- must have been a message not intended for us
         //console.log({err:err, 'event.data': event.data, status: 'Error handling event'}, '_messageHandler');
       }
-    };
+
+      if (action) {
+
+        //console.log({event: event}, 'Received FirebasePassportLogin initialization request from parent window');
+
+        if (action.redirect) {
+          self._redirectURL = action.redirect;
+        }
+
+        _getAnonymousUid()
+          .then(function (uid) {
+            self._anonymousUid = uid;
+          })
+          .catch(console.error);
+      }
+
+    }
 
     /**
+     * Initialize 
+     * 
      * Initialization occurs automatically with the setTimeout() call below,
-     * so it should not be necessary to call this method.
+     * so it should be unnecessary for other modules to call this method directly.
      */
-    self.init = function () {
+    function _init () {
 
         // Set up the message listener so that the calling window can create the
         // anonymous Firebase connection whenever the login window is opened as a dialog,
         // without reloading the page. Instead of attempting to authenticate automatically 
-        // with the passportSession whne the page is first loaded (by calling self._setToken(), 
+        // with the passportSession when the page is first loaded
+        // (by calling _getUserForTokenAndSaveSession()),
         // we allow Firebase to authenticate with the custom token that it stored from the 
         // last successful authentication.
 
@@ -228,24 +203,85 @@ var FirebasePassportLogin = (function (firebaseURL, oAuthServerURL, callback) {
             var authData = self._ref.getAuth();
             if (authData && authData.uid === storedAnonymousUid) {
                 self._anonymousUid = storedAnonymousUid;
-                self._initializePassportLogin(self._oAuthTokenPath());
+                _initializeFirebaseOAuthUserListener(_firebaseOAuthUserPath(self._anonymousUid));
             }
             else {
                 cookie.set('passportAnonymous', '');
             }
         }
         //alert('passportSession = ' + cookie.get('passportSession'));
-        window.addEventListener('message', self._messageHandler);
+        window.addEventListener('message', _messageHandler);
+    }
+
+    /**
+     * Log in to Firebase through the indicated authentication provider.
+     *
+     * First creates an anonymous authentication with Firebase, passing the anonymous auth
+     * token on to the server along with a Firebase key to set with authenticated user data.
+     * We then listen for the authenticated user data to be set, calling the user-provided
+     * `callback` function when it is.
+     *
+     * Finally we open an authentication window to a URL served by the backend auth service.
+     *
+     * The authenticated user token is stored in a cookie (`passportCookie`) to enable
+     * automatic authentication later.
+     *
+     * @param {String} provider - Provider to authenticate through ('facebook', 'reddit', etc.)
+     */
+    self.login = function (provider) {
+
+      // Have to open the authentication window immediately in order to avoid popup blockers
+
+      self._provider = provider;
+      if (! self._anonymousUid) {
+        // _getAnonymousUid() should have been called automatically when the page loaded.
+        _log("Anonymous login failed. Make sure Anonymous login is enabled in your Firebase");
+        return _getAnonymousUid().then(function (uid) {
+          // this won't work for the current attempt but might fix it for the next
+          self._anonymousUid = uid;
+          throw new Error('Anonymous UID for FireBase connection had not been established '
+            + ' but is now ready for next attempt.');
+        });
+      }
+      else {
+        // set up a Firebase listener to get the user object set by the backend auth service
+        var oAuthTokenPath = _firebaseOAuthUserPath(self._anonymousUid);
+        _initializeFirebaseOAuthUserListener(oAuthTokenPath);
+
+        // open the authentication window provided by the backend auth service
+        var oAuthWindowURL = self._oAuthServerURL + self._provider
+          + "?oAuthTokenPath=" + oAuthTokenPath
+          + "&redirect=" + encodeURIComponent(self._redirectURL);
+        if (self._firebaseURL) {
+          oAuthWindowURL += "&firebaseURL=" + encodeURIComponent(self._firebaseURL);
+        }
+        self._oAuthWindow = _popupCenter(oAuthWindowURL, "_blank",
+          self._oAuthServerWindow.width,
+          self._oAuthServerWindow.height);
+
+      }
     };
 
     /**
-     * Attempt to authenticate automatically with the passportSession cookie.
+     * Disconnect from Firebase and clear the passportSession cookie.
      */
-    setTimeout(function () {
-        self.init();
-    });
+    self.logout = function () {
+      var token = cookie.get("passportSession");
+      if (token) {
+        self._ref.child('oAuthUsers').child(token.replace(/\./g, '')).remove();
+      }
+      self._ref.unauth();
+      cookie.set("passportSession", "");
+      self._callback(null, null);
+    };
+
 
     // Copyright (c) 2012 Florian H., https://github.com/js-coder https://github.com/js-coder/cookie.js
     !function(e,t){var n=function(){return n.get.apply(n,arguments)},r=n.utils={isArray:Array.isArray||function(e){return Object.prototype.toString.call(e)==="[object Array]"},isPlainObject:function(e){return!!e&&Object.prototype.toString.call(e)==="[object Object]"},toArray:function(e){return Array.prototype.slice.call(e)},getKeys:Object.keys||function(e){var t=[],n="";for(n in e)e.hasOwnProperty(n)&&t.push(n);return t},escape:function(e){return String(e).replace(/[,;"\\=\s%]/g,function(e){return encodeURIComponent(e)})},retrieve:function(e,t){return e==null?t:e}};n.defaults={},n.expiresMultiplier=86400,n.set=function(n,i,s){if(r.isPlainObject(n))for(var o in n)n.hasOwnProperty(o)&&this.set(o,n[o],i);else{s=r.isPlainObject(s)?s:{expires:s};var u=s.expires!==t?s.expires:this.defaults.expires||"",a=typeof u;a==="string"&&u!==""?u=new Date(u):a==="number"&&(u=new Date(+(new Date)+1e3*this.expiresMultiplier*u)),u!==""&&"toGMTString"in u&&(u=";expires="+u.toGMTString());var f=s.path||this.defaults.path;f=f?";path="+f:"";var l=s.domain||this.defaults.domain;l=l?";domain="+l:"";var c=s.secure||this.defaults.secure?";secure":"";e.cookie=r.escape(n)+"="+r.escape(i)+u+f+l+c}return this},n.remove=function(e){e=r.isArray(e)?e:r.toArray(arguments);for(var t=0,n=e.length;t<n;t++)this.set(e[t],"",-1);return this},n.empty=function(){return this.remove(r.getKeys(this.all()))},n.get=function(e,n){n=n||t;var i=this.all();if(r.isArray(e)){var s={};for(var o=0,u=e.length;o<u;o++){var a=e[o];s[a]=r.retrieve(i[a],n)}return s}return r.retrieve(i[e],n)},n.all=function(){if(e.cookie==="")return{};var t=e.cookie.split("; "),n={};for(var r=0,i=t.length;r<i;r++){var s=t[r].split("=");n[decodeURIComponent(s[0])]=decodeURIComponent(s[1])}return n},n.enabled=function(){if(navigator.cookieEnabled)return!0;var e=n.set("_","_").get("_")==="_";return n.remove("_"),e},typeof define=="function"&&define.amd?define(function(){return n}):typeof exports!="undefined"?exports.cookie=n:window.cookie=n}(document);
     //
+
+    // Attempt to authenticate automatically with the passportSession cookie.
+    // This must come AFTER the cookie code pasted just above.
+    _init();
+
 });
