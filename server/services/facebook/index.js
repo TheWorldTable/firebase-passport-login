286ec5841abe16c2518d9fcea63fb56654ee2feb
Mike <mscalora@users.noreply.github.com>
2018-02-01 18:17:49 -0700

    allow extra options into facebook strategy for API changes
    Now merge config.options into config passed to passport-facebook/FacebookStrategy

--- server/services/facebook/index.js
+++ server/services/facebook/index.js
@@ -1,11 +1,11 @@
 exports.setup = function(passport, config) {
   var FacebookStrategy = require('passport-facebook').Strategy;
 
-  passport.use(new FacebookStrategy({
+  passport.use(new FacebookStrategy(Object.assign({
       clientID: config.clientID,
       clientSecret: config.clientSecret,
       callbackURL: config.callbackURL
-    },
+    }, config.options || {}),                                    
     function(accessToken, refreshToken, profile, done) {
       var user = {
         refreshToken: refreshToken || "",
