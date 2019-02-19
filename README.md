9570280546bdf61189e6e6ec25bd1564e7cb1960
Mike Scalora <mike@scalora.org>
2019-02-19 05:08:51 -0700

    Changes to initialization, added support for forceAnonymousReauth and preserveAuth options Added localStorage option to turn on debugging named 'firebase-passport-login-debug-mode' README Changes, version number change

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 ### TODO: Update instructions to reflect changes made in this fork
 
-FirebasePassportLogin (Beta!)
+FirebasePassportLogin WORLD TABLE SDK v3 FORK aka AUTH-3
 =======================
 
 Use [Express](http://expressjs.com/)'s [Passport](http://passportjs.org/) middleware authentication libraries with Firebase to authenticate users with an interface identical to Firebase Simple Login for over 100 different providers.
-
-Although Firebase Passport Login only comes with three example integrations (Reddit, Foursquare, and LinkedIn login), it is extremely easy to add any provider with a Passport library. See below for how to go about adding more services. 
-
-**Do not use Firebase Passport Login in production yet! We are still doing a lot of testing for stability and security. A production ready version is coming soon!**
 
 Getting Started
 ---
@@ -15,7 +11,7 @@ Getting Started
 ### 1. Set up Anonymous Login in your Firebase
 Open your Firebase in a browser and navigating to Simple Login tab. Then click "Anonymous" and make sure "Enabled" is checked.
 
-*Note: Firebase Passport Login uses Anonymous login interally to create a secure communication line between a Firebase Passport Login client and server.* 
+*Note: Firebase Passport Login uses Anonymous login internally to create a secure communication line between a Firebase Passport Login client and server.* 
 
 ### 2. Set up the Security Rules in your Firebase
 We've included proper security rules for using Firebase Passport Login under `/server/security-rules.json`, make sure to apply these to your Firebase. This is extremely important for keeping user tokens secure!
--- examples/server/example-server.js
+++ examples/server/example-server.js
@@ -7,8 +7,8 @@ const path = require('path'),
     http = require('http'),
     fs = require('fs'),
     options = {
-      key: fs.readFileSync(path.resolve(__dirname, 'ssl/domain.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'ssl/domain.crt')),
+      key: fs.readFileSync(path.resolve(__dirname, 'ssl/star.worldtable.co.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'ssl/star.worldtable.co.crt')),
       ca: [
         fs.readFileSync(path.resolve(__dirname, 'ssl/ca1.crt')),
         fs.readFileSync(path.resolve(__dirname, 'ssl/ca2.crt'))
@@ -17,71 +17,78 @@ const path = require('path'),
     CONFIG_DIR = process.env.TWT_CONFIG_DIR || path.join(process.env.HOME || '/root', 'twt-config'),
     admin = require('firebase-admin');
 
-program
-    .version('3.0.0')
-    .option('-p, --port <n>', 'http/https port', '443')
-    .option('-l, --level <s>', 'express & router logging level')
-    .option('--module-dev', 'use local firebase-passport-login module')
-    .option('--insecure', 'run insecure/http server')
-    .option('--test', 'run as test')
-    .option('-v, --verbose', 'verbose output mode')
-    .option('-T, --trace', 'console trace output mode')
-    .option('-P, --project <project>', 'oAuth project', 'default')
-    .option('-c, --config-dir <path>', 'configuration directory')
-    .option('-h, --auth-host <hostname>', 'alternate host name for auth server [e.g. auth2.example.com')
-    .option('--log-path <path>', 'Logfile path')
+program.version('3.1.0')
+    .option('-p, --port <n>', 'http/https port (env TWT_UATH_PORT)', process.env.TWT_AUTH_PORT || null)
+    .option('-l, --level <s>', 'express & router logging level (env TWT_AUTH_LOG_LEVEL)', process.env.TWT_AUTH_LOG_LEVEL || 'info')
+    .option('--module-dev', 'use local firebase-passport-login module (env TWT_AUTH_DEV_MODULE)')  // loads auth from ../../firebase-passport-login
+    .option('--insecure', 'run insecure/http server (env TWT_AUTH_INSECURE)')
+    .option('--test', 'run as test') // to test server connectivity
+    .option('-v, --verbose', 'verbose output mode (env TWT_AUTH_VERBOSE)')
+    .option('-c, --config-dir <path>', 'configuration directory (env TWT_CONFIG_DIR)', process.env.TWT_CONFIG_DIR || path.join(process.env.HOME || '/root', 'twt-config'))
+    .option('-H, --auth-host <hostname>', 'alternate host name for auth server [e.g. auth2.worldtable.co] (env TWT_AUTH_HOST)')
+    .option('--log-path <path>', 'Logfile path (env TWT_AUTH_LOG_PATH)', process.env.TWT_AUTH_LOG_PATH || '/tmp')
+    .option('--test', 'run as test') // to test server connectivity, uri with path /ping should respond with 'pong'
     .parse(process.argv);
 
 if (program.authHost) {
   process.env.TWT_AUTH_HOST = program.authHost;
 }
 
+if (process.env.TWT_AUTH_VERBOSE && process.env.TWT_AUTH_VERBOSE !== '0') {
+  program.verbose = true;
+}
+
+if (process.env.TWT_AUTH_INSECURE && process.env.TWT_AUTH_INSECURE !== '0') {
+  program.insecure = true;
+}
+
+if (process.env.TWT_AUTH_DEV_MODULE && process.env.TWT_AUTH_DEV_MODULE !== '0') {
+  program.moduleDev = true;
+}
+
 const express = require('express'),
     v1shim = require('oauth-shim'),
     logger = require('express-bunyan-logger'),
+    moment = require('moment'),
     config = require('./config'),
     app = express(),
+    configDir = program.configDir,
     verbose = program.verbose ? console.log.bind(console) : function () {};
 
 /********************************************************************************************
-*
-*   Config files are json files that contain the firebase project id/name "name" property
-*
-*   {
-*     "name": "production-project-id",
-*     "credentials": "production-rw.json"
-*   }
-*
-*   Service account credentials (json files form google) are in a credentials subfolder
-*   named by the "credentials" property (or named with project name)
-*
-*********************************************************************************************/
+ *
+ *   Config files are json files that contain the firebase project id/name "name" property
+ *
+ *   {
+ *     "name": "production-project-id",
+ *     "credentials": "production-rw.json"
+ *   }
+ *
+ *   Service account credentials (json files form google) are in a credentials subfolder
+ *   named by the "credentials" property (or named with project name)
+ *
+ *********************************************************************************************/
 
 /**
  * Get server-side initialization info & credentials for twt firebase project
- * @param project {String} project id, project alias or defaults to 'default'
- * @param user {String} twt userId or 'backend' or 'admin' or defaults to 'backend'
- * @param configDir {String} twt-config directory or default to home directory + 'twt-config'
+ * @param {String} project project id, project alias or defaults to 'default'
+ * @param {String} user twt userId or 'backend' or 'admin' or defaults to 'backend'
+ * @param {String} [configPath] twt-config directory or default to home directory + 'twt-config'
  * @returns {{name: String, initOptions: Object, databaseURL: string}}
  */
-function getFirebaseConfig (project, user, configDir) {
-
+function getFirebaseConfig (project, user, configPath) {
   if (!project) {
     project = process.env.TWT_PROJECT || 'default';
-  }
-
-  if (!configDir) {
-    configDir = process.env.TWT_CONFIG_DIR || path.join(process.env.HOME || '/root', 'twt-config');
   }
 
   let twtProject = {
     name: project
   };
 
-  let configPath = path.join(configDir, `${project}.json`);
+  let configFilePath = path.join(configPath, `${project}.json`);
 
-  if (fs.existsSync(configPath)) {
-    twtProject = require(configPath);
+  if (fs.existsSync(configFilePath)) {
+    twtProject = require(configFilePath);
     if (twtProject.name !== project) {
       twtProject.alias = project;
     }
@@ -91,7 +98,7 @@ function getFirebaseConfig (project, user, configDir) {
 
   twtProject.databaseURL = `https://${twtProject.name}.firebaseio.com`;
 
-  twtProject.credentialsPath = path.join(configDir, 'credentials', twtProject.credentials || twtProject.name + '.json');
+  twtProject.credentialsPath = path.join(configPath, 'credentials', twtProject.credentials || twtProject.name + '.json');
   twtProject.serviceAccount = require(twtProject.credentialsPath);
   twtProject.initOptions = {
     credential: admin.credential.cert(twtProject.serviceAccount),
@@ -124,7 +131,7 @@ function getFirebaseConnection (firebaseUrl) {
 
   verbose(`Creating new connection to ${projectId}`);
 
-  const twtProject = getFirebaseConfig(projectId, null, program.configDir || null);
+  const twtProject = getFirebaseConfig(projectId, null, configDir);
   twtProject.connection = {
     app: admin.initializeApp(twtProject.initOptions, `auth2-connection-${projectId}`),
   };
@@ -143,9 +150,9 @@ if (program.moduleDev) {
 }
 
 const firebasePassportLogin = require(firebasePassportLoginPath),
-    level = program.level || 'info',
+    level = program.level,
     port = Number(program.port || (program.insecure ? 80 : 443)),
-    logPath = program.logPath || '/var/log',
+    logPath = program.logPath,
     logFile = path.join(logPath, 'oauth-client.log');
 
 if (program.test) {
@@ -160,16 +167,19 @@ if (program.test) {
 
 } else {
 
-  // config.firebaseConfig = getFirebaseConfig(program.project);
-  // config.firebaseProject = config.firebaseConfig.name;
-
   config.connectCallback = getFirebaseConnection;
 
-  console.log(`Auth Host: ${program.authHost}`);
-  console.log(`Port: ${port}`);
-  console.log(`Secure: ${program.insecure ? 'no' : 'yes'}`);
-  console.log(`Logging: ${logFile}`);
-  console.log(`Module: ${firebasePassportLoginPath}`);
+  if (program.verbose) {
+    process.stderr.write(`Version: ${program._version}\n`);
+    process.stderr.write(`Verbose: ${program.verbose ? 'yes' : 'no'}\n`);
+    process.stderr.write(`Secure: ${program.insecure ? 'no' : 'yes'}\n`);
+    process.stderr.write(`Port: ${port}\n`);
+    process.stderr.write(`Auth Host: ${config.AUTH_HOST}\n`);
+    process.stderr.write(`Logging: ${logFile}\n`);
+    process.stderr.write(`Log Level: ${level}\n`);
+    process.stderr.write(`FPL Module: ${firebasePassportLoginPath} (${program.moduleDev ? 'dev mode' : 'from node_modules'})\n`);
+    process.stderr.write(`Config Dir: ${configDir}\n`);
+  }
 
   let router = firebasePassportLogin(config, {}),
       eLog = logger({
